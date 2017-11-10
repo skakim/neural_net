702df8f5f9c0ce9a2ee603f311a0e0fae648088b
André Driemeyer <andredriem@gmail.com>
2017-11-10 16:49:59 -0200

    progress on parser

--- argv_parser.py
+++ argv_parser.py
@@ -13,20 +13,27 @@ def parser() -> (argparse.Namespace, list):
     mode_p = mode_parser.add_subparsers(dest='mode')
     mode_p.required = True
     mode_p.add_parser('create_net',
-                      help='creates a neural_network')
+                      help='creates a neural_network, use:'
+                           'create_net -h for more details',)
     mode_p.add_parser('test', help='test a neural network given a test_set')
     mode_p.add_parser('train', help='trains a neural network given a data_set')
     mode_p.add_parser('auto_train', help='automated training')
 
     args_first, leftovers = mode_parser.parse_known_args()
 
+    # (self, input_size, output_size, hidden_layers_sizes, neurons_type = 'sigmoid', alpha = 0.0001, Lamb = 0.0)
+
     if str(args_first.mode) == 'create_net':
         second_parser = argparse.ArgumentParser(prog='create_net')
-        second_parser.add_argument('-o', nargs=1, help='directory of output', type=int, required=True)
-        second_parser.add_argument('-input_size', nargs=1, help='input size of neural net', type=int, required=True)
-        second_parser.add_argument('-output_size', nargs=1, help='number of output neurons', type=int, required=True)
-        second_parser.add_argument('-hidden_layer_sizes', nargs='+', help='sizes of hidden layer ex:\n'
+        second_parser.add_argument('-o', nargs=1, help='directory of output', required=True)
+        second_parser.add_argument('-i', '-input_size', nargs=1, help='input size of neural net', type=int, required=True)
+        second_parser.add_argument('-output_size', nargs=1, help='number of output neurons', type=int, default=[1])
+        second_parser.add_argument('-l', '-layer_sizes', nargs='+', help='sizes of hidden layer ex:\n'
                                    '-hidden_layer_sizes 4 6 8 4', type=int, required=True)
+        second_parser.add_argument('-a', '-alpha', nargs=1, type=float, default=[0.01])
+        second_parser.add_argument('-g', '-gama', nargs=1, type=float, default=[0.0])
+        second_parser.add_argument('-t', '-type', nargs=1, choices=['relu','sigmoid'], default=['sigmoid'])
+
     elif str(args_first.mode) == 'test':
         second_parser = argparse.ArgumentParser(prog='test')
         second_parser.add_argument('-i', nargs=1, help='input neural_net', required=True)
@@ -45,11 +52,3 @@ def parser() -> (argparse.Namespace, list):
     args_second, second_leftovers = second_parser.parse_known_args(leftovers)
 
     return args_first, args_second
-
-
-
-
-
-
-if __name__ == '__main__':
-    parser()
